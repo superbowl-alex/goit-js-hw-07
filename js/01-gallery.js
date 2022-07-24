d3aadd012dbf33fb9ba0378fec97fa832ddda4b5
Oleksandr <71338915+superbowl-alex@users.noreply.github.com>
2022-07-24 22:54:19 +0300

    Task01 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+galleryRef.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+galleryRef.addEventListener("click", onGalleryItemClick);
+let instance;
+
+function createGalleryMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  onModalOpen(event);
+}
+
+function onModalOpen(event) {
+  window.addEventListener("keydown", onEscKeyPress);
+  instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`);
+  instance.show();
+}
+
+function onEscKeyPress(event) {
+  if (event.code === "Escape") {
+    onModalClose();
+  }
+}
+
+function onModalClose() {
+  window.removeEventListener("keydown", onEscKeyPress);
+  instance.close();
+}
