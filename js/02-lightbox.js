6b27f62fa423cd49c5282bece25f739b78464ffb
Oleksandr <71338915+superbowl-alex@users.noreply.github.com>
2022-07-25 21:11:19 +0300

    Task02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+galleryRef.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
+
+lightbox.on("show.lightbox");
+
+function createGalleryMarkup(images) {
+  return images
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join("");
+}
